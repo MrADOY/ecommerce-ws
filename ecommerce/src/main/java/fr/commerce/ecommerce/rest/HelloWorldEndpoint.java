651e37d01ec1244f258af2daf3c91a9a979d2432
Aurelien Pietrzak <pietrzak.aurelien@gmail.com>
2020-02-18 22:16:54 +0100

    Dockerize app

--- ecommerce/src/main/java/fr/commerce/ecommerce/rest/HelloWorldEndpoint.java
+++ ecommerce/src/main/java/fr/commerce/ecommerce/rest/HelloWorldEndpoint.java
@@ -5,13 +5,31 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
+import java.sql.Connection;
+import java.sql.SQLException;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 @Path("/hello")
 public class HelloWorldEndpoint {
     @GET
     @Produces("text/plain")
-    public Response doGet() {
+    public Response doGet() throws NamingException, SQLException {
+        Context ctx = new InitialContext();
+        DataSource ds = (DataSource) ctx.lookup("jboss/datasources/EcommerceDS");
+        Connection conn = ds.getConnection();
+        try {
+
+        } finally {
+            conn.close();
+        }
         return Response.ok("Hello from Thorntail!").build();
     }
+
 }
