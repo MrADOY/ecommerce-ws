8f666df73c96669a28edf5fad0327d93c9373806
Aurelien Pietrzak <pietrzak.aurelien@gmail.com>
2020-04-16 17:01:02 +0200

    delete folder

--- banque/src/main/java/com/banque/banque/service/BankService.java
+++ banque/src/main/java/com/banque/banque/service/BankService.java
@@ -1,7 +1,5 @@
 package com.banque.banque.service;
 
-
-
 public interface BankService {
     boolean refundCustomer(String cardNumber, double amount);
     boolean debitCustomer(String cardNumber, double amount);
